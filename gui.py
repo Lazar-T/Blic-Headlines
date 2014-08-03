54b6688a26a2fcbef050050909351c64c4b668b7
Lazar-T <lazartelebak@gmail.com>
2014-08-03 22:15:12 +0200

    First commit

---
+++ gui.py
@@ -0,0 +1,73 @@
+import sys
+import time
+import webbrowser
+from PySide.QtCore import *
+from PySide.QtGui import *
+import scrapingBlic
+
+i = 1
+
+def openFirstStory():
+    webbrowser.open(href1)
+
+
+def openSecondStory():
+    webbrowser.open(href2)
+
+
+def openThirdStory():
+    webbrowser.open(href3)
+
+
+def openForthStory():
+    webbrowser.open(href4)
+
+
+def openFifthStory():
+    webbrowser.open(href5)
+
+
+text1, href1 = scrapingBlic.firstHeadline()
+text2, href2 = scrapingBlic.secondHeadline()
+text3, href3 = scrapingBlic.thirdHeadline()
+text4, href4 = scrapingBlic.forthHeadline()
+text5, href5 = scrapingBlic.fifthHeadline()
+
+
+class Form(QDialog):
+
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+
+        self.button1 = QPushButton(text1)
+        self.button2 = QPushButton(text2)
+        self.button3 = QPushButton(text3)
+        self.button4 = QPushButton(text4)
+        self.button5 = QPushButton(text5)
+
+        self.connect(self.button1, SIGNAL("clicked()"), openFirstStory)
+        self.connect(self.button2, SIGNAL("clicked()"), openSecondStory)
+        self.connect(self.button3, SIGNAL("clicked()"), openThirdStory)
+        self.connect(self.button4, SIGNAL("clicked()"), openForthStory)
+        self.connect(self.button5, SIGNAL("clicked()"), openFifthStory)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.button1)
+        layout.addWidget(self.button2)
+        layout.addWidget(self.button3)
+        layout.addWidget(self.button4)
+        layout.addWidget(self.button5)
+
+        self.setLayout(layout)
+        self.setWindowTitle("Trenutni naslovi na blic.rs")
+        self.setWindowIcon(QIcon('blic.png'))
+
+
+while True:
+    i = i + 1
+    print i
+    app = QApplication(sys.argv)
+    form = Form()
+    form.show()
+    app.exec_()
+    time.sleep(2)
